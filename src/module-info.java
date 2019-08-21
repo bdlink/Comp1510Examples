b30bf5b7cfc0c881f4d014039bfae9a19e887e11
Bruce Link <bruce_link@bcit.ca>
2019-08-20 17:24:35 -0700

    convert to modular project

---
+++ src/module-info.java
@@ -0,0 +1,20 @@
+module ca.bcit.comp1510.examplea {
+	requires javafx.controls;
+	requires javafx.fxml;
+	requires javafx.media;
+	requires transitive javafx.graphics;
+	opens ca.bcit.comp1510.ch03 to javafx.fxml;
+	exports ca.bcit.comp1510.ch03;
+	opens ca.bcit.comp1510.ch04 to javafx.fxml;
+	exports ca.bcit.comp1510.ch04;
+	opens ca.bcit.comp1510.ch05 to javafx.fxml;
+	exports ca.bcit.comp1510.ch05;
+	opens ca.bcit.comp1510.ch06 to javafx.fxml;
+	exports ca.bcit.comp1510.ch06;
+	opens ca.bcit.comp1510.ch07 to javafx.fxml;
+	exports ca.bcit.comp1510.ch07;
+	opens ca.bcit.comp1510.ch08 to javafx.fxml;
+	exports ca.bcit.comp1510.ch08;
+	opens ca.bcit.comp1510.ch09 to javafx.fxml;
+	exports ca.bcit.comp1510.ch09;
+}
