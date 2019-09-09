22ce9aa9df107b7ff610a9547a9f387332b0af9a
Bruce Link <bruce_link@bcit.ca>
2019-09-09 10:18:00 -0700

    example with var

--- src/ca/bcit/comp1510/ch02/Geometry.java
+++ src/ca/bcit/comp1510/ch02/Geometry.java
@@ -18,7 +18,7 @@ public class Geometry {
      */
     public static void main(String[] args) {
 
-        int sides = 7; // declaration with initialization
+        var sides = 7; // declaration with initialization
         System.out.println("A heptagon has " + sides + " sides.");
 
         sides = 10; // assignment statement
