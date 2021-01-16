22ba50a846abe8bfd53ec01cd825e70768159808
Bruce Link <bruce_link@bcit.ca>
2021-01-15 17:45:06 -0800

    updated .gitignore

--- src/ca/bcit/comp1510/ch08/SodaSurvey.java
+++ src/ca/bcit/comp1510/ch08/SodaSurvey.java
@@ -20,7 +20,7 @@ public class SodaSurvey {
      */
     public static void main(String[] args) {
         final int[][] scores = {{3, 4, 5, 2, 1, 4, 3, 2, 4, 4},
-                                {2, 4, 3, 4, 3, 3, 2, 1, 2, 4},
+                                {2, 4, 3, 4, 3, 3, 2, 1, 2, 2},
                                 {3, 5, 4, 5, 5, 3, 2, 5, 5, 5},
                                 {1, 1, 1, 3, 1, 2, 1, 3, 2, 4} };
 
